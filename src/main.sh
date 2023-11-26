fe9a813f8987b0510ce5e7cb7c343337f5c27cee
a-t-0 <you@example.com>
2023-11-26 01:42:13 +0100

    Changed SCRIPT_DIR global to local.

--- src/main.sh
+++ src/main.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Get the path towards this src dir.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+function load_functions() {
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/bash_logger.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/bash_logger.sh"
+
+}
+load_functions
 
 # You can set the log level using:
 #LOG_LEVEL_ALL # set log level to all, otherwise, NOTICE, INFO, DEBUG, TRACE will not be logged.
