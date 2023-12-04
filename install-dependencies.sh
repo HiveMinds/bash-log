718f730e25a2889d7188fec88c1e584031e7d3ce
a-t-0 <you@example.com>
2023-12-04 17:36:15 +0100

    Ensured dependencies and test libs submodules not committed.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,3 +14,7 @@ git submodule add --force https://github.com/sstephenson/bats test/libs/bats
 git submodule add --force https://github.com/ztombol/bats-support test/libs/bats-support
 git submodule add --force https://github.com/ztombol/bats-assert test/libs/bats-assert
 git submodule update --remote --recursive
+
+# Remove the submodules from the index.
+git rm -r -f --cached test/libs/bats
+git rm -r -f --cached dependencies
