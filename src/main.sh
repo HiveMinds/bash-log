4cecc69c0d50a7fed415bd8d4ff5ee78cffc6f86
a-t-0 <you@example.com>
2023-12-05 20:34:24 +0100

    Reduced boilerplate.

--- src/main.sh
+++ src/main.sh
@@ -1,9 +1,60 @@
 #!/bin/bash
-SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
+function load_dependencies() {
+  local calling_repo_root_path="$1"
+  local dependency_or_parent_path
+  # The path of this repo ends in /bash-create-onion-domains. If it follows:
+  # /dependencies/bash-create-onion-domains, then it is a dependency of
+  #another module.
+  echo "calling_repo_root_path=$calling_repo_root_path"
+  if [[ "$calling_repo_root_path" == *"/dependencies/bash-create-onion-domains" ]]; then
+    # This module is a dependency of another module.
+    dependency_or_parent_path=".."
+  else
+    dependency_or_parent_path="dependencies"
+  fi
+  echo "dependency_or_parent_path=$dependency_or_parent_path"
+  load_REQUIRED_DEPS "$calling_repo_root_path" "$dependency_or_parent_path"
+  load_PARENT_DEPS "$calling_repo_root_path" "$dependency_or_parent_path"
+}
+
+function load_required_dependency() {
+  local calling_repo_root_path="$1"
+  local dependency_or_parent_path="$2"
+  local dependency_name="$3"
+
+  local dependency_dir="$calling_repo_root_path/$dependency_or_parent_path/$dependency_name"
+  if [ ! -d "$dependency_dir" ]; then
+    echo "ERROR: $dependency_dir is not found in required dependencies."
+    exit 1
+  fi
+  source "$dependency_dir/src/main.sh"
+}
+
+function load_PARENT_DEPS() {
+  local calling_repo_root_path="$1"
+  local dependency_or_parent_path="$2"
+  local parent_dep="$3"
+
+  local parent_dep_dir="$calling_repo_root_path/../$parent_dep"
+  echo "parent_dep_dir=$parent_dep_dir"
+  # Check if the parent repo above the dependency dir is the parent dependency.
+  if [ ! -d "$calling_repo_root_path/../$parent_dep" ]; then
+    # Must load the dependency as any other fellow dependency if it is not
+    # a parent dependency.
+    load_required_dependency "$dependency_or_parent_path" "$required_dependency"
+  else
+    # Load the parent dependency.
+    # shellcheck disable=SC1090
+    source "$calling_repo_root_path/../$parent_dep/src/main.sh"
+  fi
+}
+
+START_TOR_AT_BOOT_SRC_PATH=$(dirname "$(readlink -f "$0")")
 function load_functions() {
 
   # shellcheck disable=SC1091
-  source "$SCRIPT_PATH/bash_logger.sh"
+  source "$START_TOR_AT_BOOT_SRC_PATH/bash_logger.sh"
 
 }
 load_functions
